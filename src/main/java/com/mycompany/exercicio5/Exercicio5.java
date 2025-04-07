5331c74e1ff629c95aeaa6f0ff81e0e1cc719939
Fabricio Valdemar de Aguiar Filho <aguiarfilho2006@gmail.com>
2025-04-07 20:31:36 -0300

    Update Exercicio5.java

--- src/main/java/com/mycompany/exercicio5/Exercicio5.java
+++ src/main/java/com/mycompany/exercicio5/Exercicio5.java
@@ -1,19 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
 package com.mycompany.exercicio5;
-
-/**
- *
- * @author Fabricio de Aguiar
- */
-/**Faça um programa em JAVA, declarando
-uma variável do tipo inteiro (int), atribua um
-valor qualquer a esta variável, depois
-mostre o valor desta variável em uma
-mensagem.*/
-
 import javax.swing.JOptionPane;
 public class Exercicio5 {
 
